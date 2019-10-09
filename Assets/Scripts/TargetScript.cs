e492684399867a41511b7b1cc0f1f85a88e347ff
tedglim <tedlim@alumni.stanford.edu>
2019-10-09 15:19:39 -0700

    First commit on targets
    add dummy target. edit plugins on C# editor

---
+++ Assets/Scripts/TargetScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnParticleCollision(GameObject particle)
+    {
+        print("object should be destroyed.");
+        print("message some manager");
+        print("destroy gameobject");
+    }
+}
